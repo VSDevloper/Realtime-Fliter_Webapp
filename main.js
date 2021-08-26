03af2649109035eac62b0783e43e66ca31403f72
VSDevloper <84179803+VSDevloper@users.noreply.github.com>
2021-08-26 19:17:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,3 +19,22 @@ function setup() {
 function modelLoaded() {
   console.log('PoseNet Is Initialized');
 }
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x-15;
+    noseY = results[0].pose.nose.y-15;
+  }
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function take_snapshot(){    
+  save('myFilterImage.png');
+}
